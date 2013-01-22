babb6acdc1cfef000edec932322f3aa926ba8ab2
Chris Crook <ccrook@linz.govt.nz>
2013-01-22 14:27:55 +1300

    WIP

--- VectorFieldRendererScaleBox.py
+++ VectorFieldRendererScaleBox.py
@@ -274,4 +274,81 @@ class VectorFieldRendererScaleBox:
 
        
 
+class VectorScaleBoxPluginLayer(QgsPluginLayer):
+
+  LAYER_TYPE="vectorscalebox"
+
+  def __init__(self):
+    QgsPluginLayer.__init__(self, VectorScaleBoxPluginLayer.LAYER_TYPE, "Vector scale box plugin layer")
+    self.setValid(True)
+
+    self.image = None
+
+  def draw(self, rendererContext):
+    if self.image != None:
+      painter = rendererContext.painter()
+      extent = rendererContext.extent()
+      mapToPixel = rendererContext.mapToPixel()
+      rasterScaleFactor = rendererContext.rasterScaleFactor()
+      invRasterScaleFactor = 1.0/rasterScaleFactor
+
+      # get dimensions of painter area (so it is also correctly scaled in print composer)
+      # Note: watermark is not correctly scaled in print preview
+      topleft = mapToPixel.transform(extent.xMinimum(), extent.yMaximum())
+      bottomright = mapToPixel.transform(extent.xMaximum(), extent.yMinimum())
+      width = (bottomright.x() - topleft.x()) * rasterScaleFactor
+      height = (bottomright.y() - topleft.y()) * rasterScaleFactor
+
+      # setup painter
+      painter.save()
+      painter.scale(invRasterScaleFactor, invRasterScaleFactor)
+
+      # render watermark image in lower left corner
+      painter.setOpacity(0.5)
+      y = height - 32 - self.image.height()
+      painter.drawImage(32, y, self.image)
+
+      painter.restore()
+    return True
+
+  def readXml(self, node):
+    # custom properties
+    self.readImage( node.toElement().attribute("image_path", ".") )
+    return True
+
+  def writeXml(self, node, doc):
+    element = node.toElement();
+    # write plugin layer type to project  (essential to be read from project)
+    element.setAttribute("type", "plugin")
+    element.setAttribute("name", VectorScaleBoxPluginLayer.LAYER_TYPE);
+    # custom properties
+    element.setAttribute("image_path", str(self.image_path))
+    return True
+
+  def readImage(self, image_path):
+    self.image_path = image_path
+    self.image = QImage(self.image_path)
+
+  def showDialog(self):
+    filename = QFileDialog.getOpenFileName(None, "Select watermark image", self.image_path, "Image Files (*.png *.jpg *.bmp)")
+    if filename != "":
+      self.readImage(filename)
+
+      # trigger repaint
+      self.setCacheImage(None)
+      self.emit(SIGNAL("repaintRequested()"))
    
+
+class VectorScaleBoxPluginLayerType(QgsPluginLayerType):
+
+  def __init__(self):
+    QgsPluginLayerType.__init__(self, VectorScaleBoxPluginLayer.LAYER_TYPE)
+
+  def createLayer(self):
+    return VectorScaleBoxPluginLayer()
+
+  def showLayerProperties(self, layer):
+    layer.showDialog()
+
+    # indicate that we have shown the properties dialog
+    return True
